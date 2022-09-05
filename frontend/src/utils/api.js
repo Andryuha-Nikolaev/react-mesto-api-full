a29016c4dfce61358e93cf74de1dbcb3d20a80de
admin <andryuha_nikolaev@mail.ru>
2022-09-05 13:41:58 +0300

    fix: исправления после ревью 2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,16 +1,18 @@
 import checkResponse from "./utils";
 
 class Api {
-  constructor(options) {//конструктор принимает url сервера и заголовки запроса
+  constructor(options) {//конструктор принимает url
     this._url = options.baseUrl;
-    this._headers = options.headers;
   }
 
   // метод изменяет данные профиля на сервере
   setUserInfo(data) {
     return fetch(`${this._url}/users/me`, {
       method: 'PATCH',//метод запроса
-      headers: this._headers,//заголовки запроса
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },//заголовки запроса
       body: JSON.stringify({//тело запроса
         name: data.name,//в name передаем значение name объекта, переданного в setUserInfo
         about: data.about//в about передаем значение about объекта, переданного в setUserInfo
@@ -22,7 +24,10 @@ class Api {
   setUserAvatar(data) {
     return fetch(`${this._url}/users/me/avatar`, {
       method: 'PATCH',
-      headers: this._headers,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         avatar: data.avatar
       })
@@ -34,7 +39,6 @@ class Api {
     return fetch(`${this._url}/users/me`, {
       method: 'GET',
       headers: {
-        // authorization: '6634f396-3fbd-4a4f-858c-8c72fb67fb49',
         Authorization: `Bearer ${localStorage.getItem('jwt')}`,
         'Content-Type': 'application/json'
       }
@@ -46,7 +50,6 @@ class Api {
     return fetch(`${this._url}/cards`, {
       method: 'GET',
       headers: {
-        // authorization: '6634f396-3fbd-4a4f-858c-8c72fb67fb49',
         Authorization: `Bearer ${localStorage.getItem('jwt')}`,
         'Content-Type': 'application/json'
       }
@@ -57,7 +60,10 @@ class Api {
   postCard(data) {
     return fetch(`${this._url}/cards`, {
       method: 'POST',
-      headers: this._headers,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         name: data.name,
         link: data.link
@@ -69,7 +75,10 @@ class Api {
   deleteCard(cardId) {
     return fetch(`${this._url}/cards/${cardId}`, {
       method: 'DELETE',
-      headers: this._headers,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
     }).then((res) => checkResponse(res))
   }
 
@@ -77,19 +86,16 @@ class Api {
   changeLikeCardStatus(cardId, isLiked) {
     return fetch(`${this._url}/cards/${cardId}/likes`, {
       method: `${!isLiked ? 'DELETE' : 'PUT'}`,
-      headers: this._headers,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json'
+      },
     }).then((res) => checkResponse(res))
   }
 }
 
 const api = new Api({
-  // baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-42',
-  baseUrl: 'https://api.domainname.andrey.nomoredomains.sbs',
-  headers: {
-    // authorization: '6634f396-3fbd-4a4f-858c-8c72fb67fb49',
-    Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-    'Content-Type': 'application/json'
-  }
+  baseUrl: 'https://api.domainname.andrey.nomoredomains.sbs'
 });
 
 export default api;
